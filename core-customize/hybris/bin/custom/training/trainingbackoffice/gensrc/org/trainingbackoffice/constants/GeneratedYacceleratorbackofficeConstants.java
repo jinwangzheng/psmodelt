64fe425f0fcf6ee13ceb45bcb146e1ecf80fd25e
Brook Jin <brook.jin@sap.com>
2021-02-11 18:09:59 +0800

    custom localextensions.xml and manifest.json

---
+++ core-customize/hybris/bin/custom/training/trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Feb 11, 2021, 1:08:16 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
